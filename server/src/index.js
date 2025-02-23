f6ad8f777c9ef840085a5535c0e13d0e71a6cb5e
mohnishsingh yadav <132808264+Mohnishsingh998@users.noreply.github.com>
2025-02-23 17:42:32 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -25,6 +25,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/client/dist")));
   app.get("*", (req, res) => {
