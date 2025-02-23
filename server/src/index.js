8be7797206a175633d16ec3585c5b56318ec6c61
mohnishsingh yadav <132808264+Mohnishsingh998@users.noreply.github.com>
2025-02-23 17:24:29 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -26,9 +26,9 @@ app.use("/api/messages", messageRoutes);
 
 const PORT = process.env.PORT;
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/dist")));
+  app.use(express.static(path.join(__dirname, "../client/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/client/dist/index.html"));
+    res.sendFile(path.join(__dirname, "../client/dist","/index.html"));
   });
 }
 server.listen(PORT, () => {
